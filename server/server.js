1830b962895e60744ab7d0b56b0053e0a9816e1d
Vivek <vsnits4002@gmail.com>
2026-07-02 06:13:23 +0000

    Initial backend setup: Express server connected to MongoDB

---
+++ server/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express=require('express');
+const mongoose=require('mongoose')
+const cors=require('cors');
+
+const app=express();
+app.use(cors());
+app.use(express.json()) 
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected');
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.log('not connected', err);
+  }
+}
+
+startServer(); 
+
+
